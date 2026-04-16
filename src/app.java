b2d9d149339ca0f5d711e425a31566ff354ae816
Fion <ilavolovik279@gmail.com>
2026-04-16 16:33:29 +0300

    Четверта домашка

--- src/app.java
+++ src/app.java
@@ -3,45 +3,29 @@ public class app {
     static void main(String[] args) {
         System.out.println("Shop-online for pokupatel");
         orderNr1();
+        System.out.println();
+
         System.out.println("===================");
         System.out.println("===================");
         orderNr2();
 
     }
 
-    public static void orderNr1(){
-        String part1_of_orderClient = "Order No ";
-        int numberClient = 1;
-        String part2_of_orderClient = " Client: Alice.";
-        System.out.println(part1_of_orderClient + numberClient + part2_of_orderClient);
-
-
-        String product = "smartphone.";
-        System.out.println("Product: " + product);
-
-        double price = 399.99;
-        System.out.println("price EUR " + price);
-
-        String adress = "Moon Street, ";
-        int numberOfAdress = 10;
-        System.out.println("Adress: " + adress + numberOfAdress);
+    public static void orderNr2(){
+        double laptopCost = 625.53;
+        double daySale = 35 * laptopCost;
+        double sevenDaySale = 7 * daySale;
+        System.out.printf("total sales for 7 days is EUR %.2f", sevenDaySale);
+        System.out.println();
+        System.out.printf("sales by day is EUR %.2f", daySale);
     }
 
-    public static void orderNr2(){
-        String part1_of_orderClient = "Order No ";
-        int numberClient = 2;
-        String part2_of_orderClient = " Client: Tom.";
-        System.out.println(part1_of_orderClient + numberClient + part2_of_orderClient);
-
-
-        String product = "laptop.";
-        System.out.println("Product: " + product);
-
-        double price = 570.95;
-        System.out.println("price EUR " + price);
-
-        String adress = "Terra Street, ";
-        int numberOfAdress = 17;
-        System.out.println("Adress: " + adress + numberOfAdress);
+    public static void orderNr1(){
+        double laptopCost = 513.48;
+        double daySale = 14 * laptopCost;
+        double fiveDaySale = 5 * daySale;
+        System.out.printf("total sales for 5 days is EUR %.2f", fiveDaySale);
+        System.out.println();
+        System.out.printf("sales by day is EUR %.2f", daySale);
     }
 }
